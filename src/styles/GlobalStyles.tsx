ac0044c34c891aaab5c2318993034b2d64c1bf09
koomekevo <koome.kevo@gurucrafts.net>
2024-05-18 22:22:34 +0300

    Add Colors.ts and GlobalStyles.tsx

---
+++ src/styles/GlobalStyles.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+import { myColors } from "./Colors";
+
+export const Styles = StyleSheet.create({
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: myColors.blue,
+  },
+});
