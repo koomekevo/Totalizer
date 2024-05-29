a3965329515840c74fe70a8edbf502c11185dcf4
koomekevo <koome.kevo@gurucrafts.net>
2024-05-29 21:14:01 +0300

    Modify App.tsx

--- App.tsx
+++ App.tsx
@@ -1,14 +1,14 @@
-import { useState, useContext } from "react";
-import { StyleSheet, Switch, Text, View } from "react-native";
-import Button from "./src/components/Button";
+import { useState } from "react";
+import { SafeAreaView, StyleSheet, Switch, Text } from "react-native";
 import { myColors } from "./src/styles/Colors";
 import { ThemeContext } from "./src/context/ThemeContext";
+import MyKeyboard from "./src/components/MyKeyboard";
 
 export default function App() {
   const [theme, setTheme] = useState("light");
   return (
     <ThemeContext.Provider value={theme}>
-      <View
+      <SafeAreaView
         style={
           theme === "light"
             ? styles.container
@@ -19,10 +19,8 @@ export default function App() {
           value={theme === "dark"}
           onValueChange={() => setTheme(theme === "light" ? "dark" : "light")}
         />
-        <Button title="1" isBlue onPress={() => alert("Hello")} />
-        <Button title="1" isGray onPress={() => alert("Hello")} />
-        <Button title="1" onPress={() => alert("Hello")} />
-      </View>
+        <MyKeyboard />
+      </SafeAreaView>
     </ThemeContext.Provider>
   );
 }
@@ -32,6 +30,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: myColors.light,
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "flex-start",
   },
 });
